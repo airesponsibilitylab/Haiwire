72ba52027ab5a7457ab8a0f26a6521aa8072f8a4
Alex <alex@airesponsibilitylab.com>
2024-01-21 16:58:03 -0800

    initial commit
    First commit
    - skeleton code notes

---
+++ Haiwire.py
@@ -0,0 +1,37 @@
+from math import random
+import time
+
+#read incidents - do we want to hard-code these or read from a file so its editable? Code is editable...
+with open (incients.txt)
+
+# read inject cards
+with open("inject.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+# Need:
+# - Random number generator to generate a whole # in the range of the amount of cards in the deck (dont need to worry about shuffling the decks if theyre all being picked via RNG)
+# - timer between draws (set via input 'easy'/'medium'/'hard' - default 'Medium' w/ empty string enter)
+# - Game status:
+# 	- round counter
+# 	- inject 
+# - 2 dictionary of objects:
+# 	Incident Card Dict:
+# 		- Unique card #ID
+# 		- Bool to track if its been drawn already
+# 		- Color (R/Y/G) corresponding to the "inject card" value/count (int 2-4)
+# 		- Description
+# 	Inject Card Dict:
+# 		- Unique Inject card #ID
+# 		- Bool to track if its been drawn already (are these single-use?)
+# 		- Description
+
