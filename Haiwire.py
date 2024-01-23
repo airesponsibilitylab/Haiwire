0312dbc4e52e6a2746cee7a3290701a47f2ef55c
Alex <alex@airesponsibilitylab.com>
2024-01-23 15:35:48 -0800

    almost done
    almost done

--- Haiwire.py
+++ Haiwire.py
@@ -9,7 +9,7 @@ def init():
 		incidentReader = csv.DictReader(incidentRead)
 		for row in incidentReader:
 			incidentsDict = {}
-			incidentsDict["Incident ID"] = row["General Deck ID"]
+			incidentsDict["Incident ID"] = int(row["General Deck ID"])
 			incidentsDict["Incident Title"] = row["Card Title"]
 			incidentsDict["Incident Description"] = row["Card Wording"]
 			incidentsDict["Incident Tech"] = row["AI Stack"]
@@ -30,44 +30,53 @@ def init():
 		injectReader = csv.DictReader(injectRead)
 		for row in injectReader:
 			injectDict = {}
-			injectDict["Inject ID"] = row["Deck ID"]
+			injectDict["Inject ID"] = int(row["Deck ID"])
 			injectDict["Inject Title"] = row["Card Title"]
 			injectDict["Inject Wording"] = row["Card Wording"]
 			injectList.append(injectDict)
+
+	return incidentsList,injectList
 
 #DONE
 
 #---
 
 def welcome():
-	print("Welcome to HAIWIRE, a command-line based card game about 'AI Incidents' and how teams might react to the news.\n\nINSTRUCTIONS: One Incident Card is drawn per round. Each Card is assigned a color value that determines how many 'Inject Cards' are then drawn that you then have to individually address the consequences of as they pertain to the theme of the Incident. You have two minutes to come up with a plan for each Inject Card before the Die is rolled. \n\nA Die is rolled to determine whether or not your team's 'incident response' is up to muster - Cross your fingers and hope for a 10 otherwise you risk going HAIWIRE!\n\nEach round ends once all Inject Cards for that Incident have been dealt with. The game is complete once all Incidents have been addressed.\n\n**--------------**\n")
+	print("Welcome to HAIWIRE, a command-line based card game about 'AI Incidents' and how teams might react to the news.\n\nINSTRUCTIONS: One Incident Card is drawn per round. Each Card is assigned a color value that determines how many 'Inject Cards' are then drawn that you then have to individually address the consequences of as they pertain to the theme of the Incident. You have two minutes to come up with a plan for each Inject Card before the Die is rolled. \nA Die is rolled to determine whether or not your team's 'incident response' is up to muster - Cross your fingers and hope for a 10 otherwise you risk going HAIWIRE!\n\nEach round ends once all Inject Cards for that Incident have been dealt with. The game is complete once all Incidents have been addressed.\n\n**--------------**")
 
 #Done
 
 #---
 
 def roll():
-	result = int(math.random(1,10))
+	rollResult = int(random.randrange(1,10))
+	print("You rolled a {}".format(str(rollResult)))
+	statusMessage = "fmndklgmdfgkld"
+	haiwireVal = 0
 	if rollResult < 7:
-		haiwireCount +=1
-		status = "Incident response failed, your HAIWIRE risk has increased! Please draw a new Inject card."
-	elif rollResult >= 8 and rollResult <10:
-		status = "Incident response partially successful, your HAIWIRE risk remains the same. Please draw a new Inject card."
+		haiwireVal +=1
+		statusMessage = "Incident response failed, your HAIWIRE risk has increased! Please draw a new Inject card."
+	elif rollResult >= 7 and rollResult <10:
+		statusMessage = "Incident response partially successful, your HAIWIRE risk remains the same. Please draw a new Inject card."
+		haiwireVal = 0
 	elif rollResult == 10:
-		status = "HAIWIRE averted! Please draw a new Incident card."
-	return status
+		statusMessage = "HAIWIRE averted! Please draw a new Incident card."
+		haiwireVal = -1
+	else:
+		return
+
+
+	return statusMessage, haiwireVal
 
 
 
 #---
 
 def drawIncident():
-	ID = int(random.randrange(1,20))
-	for datadict in incidentsList:
-		if datadict["Incident ID"] == ID:
-			ID = index(datadict["Incident ID"])
-			ID = index(datadict["Inject ID"])
-			incidentData = incidentsList.pop(ID)
+	ID = int(random.randrange(1,21))
+	for index, datadict in enumerate(incidentsList):
+		if ID == int(datadict["Incident ID"]):
+			incidentData = incidentsList.pop(index)
 			return incidentData
 		else:
 			return None
@@ -76,35 +85,39 @@ def drawIncident():
 #---
 
 def drawInject():
-	ID = int(random.randrange(1,40))
-	for datadict in injectList:
-		if datadict["Inject ID"] == ID:
-			ID = index(datadict["Inject ID"])
-			injectData = injectList[ID]
+	ID = int(random.randrange(1,41))
+	for index, datadict in enumerate(injectList):
+		if ID == int(datadict["Inject ID"]):
+			injectData = datadict
 			return injectData
 
 #DONE
 
 #---
-def run():
+def run(incidentsList,injectList):
+	time.sleep(1)
 	deckTotal = len(incidentsList)
+	current = 0
 	while deckTotal > 0:
+		haiwireCount = 0
 		deckTotal -= 1
-		
+		current +=1
+		print("Round {}!".format(str(current)))
 		#start round
 		incidentData = None
 		while incidentData == None:
 			incidentData = drawIncident()
-		print("Incident Title: {}\n".format(incidentData["Incident Title"]))
-		print("Incident Description: {}\n".format(incidentData["Incident Description"]))
-		print("Incident Technology: {}\n".format(incidentData["Incident Tech"]))
+		print("Incident Title: {}".format(incidentData["Incident Title"]))
+		print("Incident Description: {}".format(incidentData["Incident Description"]))
+		print("Incident Technology: {}".format(incidentData["Incident Tech"]))
 		print("Incident Domain: {}\n".format(incidentData["Incident Domain"]))
-		print("Incident Harm: {}\n\n".format(incidentData["Incident Harm"]))
+		print("Incident Harm: {}".format(incidentData["Incident Harm"]))
 		print("Incident Severity: {}".format(incidentData["Incident Color"]))
 		print()
 		injectTotal = incidentData["Incident Inject Count"]
-		print("Discuss your Response Plan and then draw {} injects. Press 'Enter/Return' to draw an Inject Card.\n\n\n".format(str(injectTotal)))
-		input = ""
+		print("Discuss your Response Plan and then draw {} injects. Press 'Enter/Return' to draw an Inject Card.\n**-------**\n".format(str(injectTotal)))
+		stop = input("")
+		time.sleep(1)
 		
 		#start inject card draw
 		injectCount = 0
@@ -114,25 +127,32 @@ def run():
 			print("Inject Card Title: {}".format(injectData["Inject Title"]))
 			print("Inject Card Description: {}".format(injectData["Inject Wording"]))
 			print()
-			print("Discuss the Inject Card, updating or reformulating your response strategy. You have two minutes starting *now*.")
-			time.sleep(120)
-			print()
-
-			#roll
-			injectRemaining = injectTotal - injectCount #4-1 = 3
-			roll(injectCardsRemaining)
-
-
-
-		print(rollResult)
+			print("Discuss the Inject Card, updating or reformulating your response strategy. You have two minutes starting *now*.\n")
+			time.sleep(5)
+			print("Rolling a Die!")
+			statusMessage,haiwireVal = roll()
+			haiwireCount += haiwireVal
+			if haiwireVal == -1:
+				print(statusMessage)
+				break
+			elif haiwireVal == 0:
+				print(statusMessage)
+				print
+			elif haiwireVal == 1:
+				print(statusMessage)
+				print(haiwireCount)
+				if haiwireCount == injectTotal:
+					print("You lose the round! Your org has gone HAIWIRE!!")
+		if haiwireCount != injectTotal:
+			print("Round over, your HAIWIRE score was: {}".format(haiwireCount))
 
 
 #---
 
-init()
+incidentsList,injectList = init()
 welcome()
-run()
-
+run(incidentsList,injectList)
+exit()
 
 
 
