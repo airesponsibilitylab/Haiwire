32efbc388525e161d46fbd2f69a3392de94a1a20
Alex <alex@airesponsibilitylab.com>
2024-01-23 17:58:24 -0800

    final update
    It lives!

--- Haiwire.py
+++ Haiwire.py
@@ -43,29 +43,30 @@ def init():
 
 def welcome():
 	print("Welcome to HAIWIRE, a command-line based card game about 'AI Incidents' and how teams might react to the news.\n\nINSTRUCTIONS: One Incident Card is drawn per round. Each Card is assigned a color value that determines how many 'Inject Cards' are then drawn that you then have to individually address the consequences of as they pertain to the theme of the Incident. You have two minutes to come up with a plan for each Inject Card before the Die is rolled. \nA Die is rolled to determine whether or not your team's 'incident response' is up to muster - Cross your fingers and hope for a 10 otherwise you risk going HAIWIRE!\n\nEach round ends once all Inject Cards for that Incident have been dealt with. The game is complete once all Incidents have been addressed.\n\n**--------------**")
-
+	time.sleep(10)
 #Done
 
 #---
 
 def roll():
 	rollResult = int(random.randrange(1,10))
+	time.sleep(2)
 	print("You rolled a {}".format(str(rollResult)))
-	statusMessage = "fmndklgmdfgkld"
-	haiwireVal = 0
+	time.sleep(1)
+	statusMessage = "fmndklgmdfgkld FAILURE AT STATUS MESSAGE"
 	if rollResult < 7:
-		haiwireVal +=1
-		statusMessage = "Incident response failed, your HAIWIRE risk has increased! Please draw a new Inject card."
+		haiwireVal = 1
+		statusMessage = "Incident response failed, your HAIWIRE risk has increased! Press 'Enter/Return' to draw a new Inject Card."
 	elif rollResult >= 7 and rollResult <10:
-		statusMessage = "Incident response partially successful, your HAIWIRE risk remains the same. Please draw a new Inject card."
+		statusMessage = "Incident response partially successful, your HAIWIRE risk remains the same. Press 'Enter/Return' to draw a new Inject Card."
 		haiwireVal = 0
 	elif rollResult == 10:
-		statusMessage = "HAIWIRE averted! Please draw a new Incident card."
+		statusMessage = "Inject Card catastrophe averted! Press 'Enter/Return' to draw a new Inject Card."
 		haiwireVal = -1
 	else:
 		return
 
-
+	# time.sleep(1)
 	return statusMessage, haiwireVal
 
 
@@ -115,23 +116,24 @@ def run(incidentsList,injectList):
 		print("Incident Severity: {}".format(incidentData["Incident Color"]))
 		print()
 		injectTotal = incidentData["Incident Inject Count"]
-		print("Discuss your Response Plan and then draw {} injects. Press 'Enter/Return' to draw an Inject Card.\n**-------**\n".format(str(injectTotal)))
+		print("Discuss your Response Plan and then draw {} injects. Press 'Enter/Return' to draw an Inject Card.\n**-------**".format(str(injectTotal)))
 		stop = input("")
 		time.sleep(1)
 		
 		#start inject card draw
 		injectCount = 0
 		while injectCount < injectTotal:
+			time.sleep(1)
 			injectData = drawInject()
 			injectCount+=1
 			print("Inject Card Title: {}".format(injectData["Inject Title"]))
 			print("Inject Card Description: {}".format(injectData["Inject Wording"]))
 			print()
-			print("Discuss the Inject Card, updating or reformulating your response strategy. You have two minutes starting *now*.\n")
-			time.sleep(5)
+			print("Discuss the Inject Card and update or reformulate your response strategy. Press 'Enter/Return' to roll a Die: \n")
+			stop = input("")
 			print("Rolling a Die!")
 			statusMessage,haiwireVal = roll()
-			haiwireCount += haiwireVal
+			haiwireCount += 1
 			if haiwireVal == -1:
 				print(statusMessage)
 				break
@@ -140,49 +142,26 @@ def run(incidentsList,injectList):
 				print
 			elif haiwireVal == 1:
 				print(statusMessage)
-				print(haiwireCount)
 				if haiwireCount == injectTotal:
 					print("You lose the round! Your org has gone HAIWIRE!!")
-		if haiwireCount != injectTotal:
-			print("Round over, your HAIWIRE score was: {}".format(haiwireCount))
-
+			print("\n**-------**\n")
+			stop = input("")
+			if haiwireCount == injectTotal:
+				time.sleep(1)
+				print("Round over, your HAIWIRE score was: {} of {} points".format(haiwireCount,injectTotal))
+				break
+		print()
+		print("\n\n**--------------**")
+	time.sleep(5)
+	print("\n\n**--------------**")
+	print("Game over! Thanks for playing!\n\n")
+	print("\n\n**--------------**")
+	time.sleep(5)
 
 #---
 
-incidentsList,injectList = init()
-welcome()
-run(incidentsList,injectList)
-exit()
-
-
-
-
-
-# This doesn't need to be "Alex thorough" for mechanics. Just "playable". It's okay if certain 
-# things aren't developed/tracked in-game. It just needs to run.
-
-# Need:
-# - Random number generator to generate a whole # in the range of the amount of cards in the deck (dont need to worry about shuffling the decks if theyre all being picked via RNG)
-# - timer between draws (set via input 'easy'/'medium'/'hard' - default 'Medium' w/ empty string enter)
-		# ASK RAMSAY ABOUT THE DIFFICULTY/TIME PER INJECT
-# - We're going with a specific # of incidents to track progress in the game/"when does the game end"
-# - Game status:
-# 		- round counter / checks at the end of each round to see if that's the last round
-# - 2 dictionary of objects:
-#	 	Incident Card Dict:
-# 			- Unique card #ID
-# 			- Bool to track if its been drawn already
-#	 		- Color (R/Y/G) corresponding to the "inject card" value/count (int 2-4)
-# 			- Description
-# 		Inject Card Dict:
-# 			- Unique Inject card #ID
-# 			- Bool to track if its been drawn already (are these single-use?)
-# 			- Description
-
-
-# Game steps:
-
-# - input stop
-# - prompt player to hit enter to roll a die
-# - generate random # 1-10
-# - display result
+while True:
+	incidentsList,injectList = init()
+	welcome()
+	run(incidentsList,injectList)
+	exit()
